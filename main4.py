63d394cc933d41a7518c1c94314d02dd833b36c7
mannandesai1 <113947752+mannandesai1@users.noreply.github.com>
2022-10-02 23:25:15 -0500

    Add files via upload

---
+++ main4.py
@@ -0,0 +1,7 @@
+LastName = input("Enter your last name")
+CreditsTaken = float(input("Enter your total credits taken"))
+
+Tuition = (CreditsTaken * 250 +100)
+
+print("your last name", LastName)
+print("Your total tuition is $", Tuition)
