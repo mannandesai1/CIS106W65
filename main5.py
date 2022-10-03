477b4a6075102630afb17afe4005e364f07a8ec5
mannandesai1 <113947752+mannandesai1@users.noreply.github.com>
2022-10-03 00:26:49 -0500

    Add files via upload

---
+++ main5.py
@@ -0,0 +1,6 @@
+PriceofItem = float(input("Please enter the price of item"))
+DiscountPercent = print(input("Discount percentage is 30%"))
+DiscountAmount = (PriceofItem * 0.30)
+DiscountPrice = PriceofItem - DiscountAmount
+print("The discounted amount is", DiscountAmount)
+print("The discounted price is", DiscountPrice)
