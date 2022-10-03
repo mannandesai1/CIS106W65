1aaaa306f1239551d18d4f9efe6cfe36fa6f65a9
mannandesai1 <113947752+mannandesai1@users.noreply.github.com>
2022-10-02 23:00:01 -0500

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,11 @@
+#this is a comment
+Length = float(input("Please enter the length of rectangle"))
+Width = float(input("Please enter the width of rectangle"))
+
+#process phase
+Area = Length * Width
+Circumference = (2 * Length) + (2 * Width)
+
+#output phase
+print("The area is", Area)
+print("The circumference is", Circumference)
