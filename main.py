0856c11c60741d0ad77c1753dafdee4cc37bbeee
mannandesai1 <113947752+mannandesai1@users.noreply.github.com>
2022-10-02 19:14:11 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+#this is a comment
+Quantity = float(input("Please enter quantity ordered"))
+PricePerUnit = float(input("Please enter price per unit"))
+
+#process phase
+ExtendedPrice = Quantity*PricePerUnit
+
+#output phase
+print("The extended price is $", ExtendedPrice)
