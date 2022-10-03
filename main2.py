ff91e8e8fe78aef4188e740116ad01fdb5352678
mannandesai1 <113947752+mannandesai1@users.noreply.github.com>
2022-10-02 19:12:55 -0500

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,8 @@
+LastName = input("Enter your last name")
+Hours = float(input("Enter hours worked"))
+Rate = float(input("Enter rate of pay"))
+
+GrossPay = Hours*Rate
+
+print(LastName)
+print("Your GrossPay is $", GrossPay)
