ba96aced3e04bb44a1562f489f84e88caa394fa8
mannandesai1 <113947752+mannandesai1@users.noreply.github.com>
2022-10-27 21:53:47 -0500

    Add files via upload

---
+++ ps5p2.py
@@ -0,0 +1,7 @@
+StartValue = int(input("Enter a start value"))
+StopValue = int(input("Enter a stop value"))
+IncrementValue = int(input("Enter an increment value"))
+
+while StartValue <= StopValue:
+  print(StartValue)
+  StartValue = StartValue + IncrementValue
